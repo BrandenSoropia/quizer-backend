8e3cf85d2f31b56b308a4b45b50eac2f5ce94fa4
Jessy <liangjessy@gmail.com>
2018-06-04 23:07:22 -0400

    Revert "Merge branch 'master' of https://github.com/BrandenSoropia/quizer-backend"
    This reverts commit 5e6c848f0b8509e6312e341b14f55c33d0d401c1, reversing
    changes made to 95cc2892ccca48f411cde9f241e5e7f009cad29b.

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ const quizzes = require('./routes/quizzes');
 const userQuizzes = require('./routes/userQuizzes');
 
 const app = express();
+
 // Connect to appropriate DB based on env
 // TODO: Update to read from env variable
-mongoose.connect(process.env.DB_URL)
+mongoose.connect('mongodb://127.0.0.1:27017/test')
   .then(
     function() { // Connect success
       console.log('Connection successful')
